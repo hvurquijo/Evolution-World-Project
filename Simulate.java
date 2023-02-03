e690f739396c5b0df75b0a71c1a387aab9af1013
Hermes A. V. Urquijo <hvurquijo@gmail.com>
2023-02-03 11:17:22 -0300

    new arquivo

---
+++ Simulate.java
@@ -0,0 +1,8 @@
+public class Simulate {
+    public int NUMBER_OF_CRIATURES=1000;
+    public int NUMBER_OF_FOOD_PARTICLES = NUMBER_OF_CRIATURES*3;
+    public long TIME_OF_EVOLUTION = 100000;
+    public static void main(String[] args) {
+        
+    }
+}
